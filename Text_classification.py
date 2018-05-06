533600d94f10a5bb025aae80f3667c6dadf30d51
Heikki Niittylä <31819037+heiniit@users.noreply.github.com>
2018-05-06 13:13:32 +0300

    Add files via upload

---
+++ Text_classification.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 12:31:23 2018
+
+@author: Heikki
+"""
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Training data
+categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+newsgroups_train = fetch_20newsgroups(subset='train',categories=categories)
+
+# Vectorize and normalize
+vectorizer = TfidfVectorizer()
+vectors = vectorizer.fit_transform(newsgroups_train.data)
+
+# Train naive Bayes
+classifier = MultinomialNB()
+classifier.fit(vectors, newsgroups_train.target)
+
+# Test the classifier with some strings
+test_strings = ["God is love", 
+                "OpenGL on the GPU is fast", 
+                "I'm an atheist", 
+                "Medical students are specialized"]
+test_vector = vectorizer.transform(test_strings)
+predicted = classifier.predict(test_vector)
+
+# Print test classification
+for doc, category in zip(test_strings, predicted):
+    print('%r => %s' % (doc, newsgroups_train.target_names[category]))
