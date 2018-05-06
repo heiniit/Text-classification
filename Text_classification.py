6dffe9a4f72df22ed76466b003d8507b41b2839b
Heikki Niittylä <31819037+heiniit@users.noreply.github.com>
2018-05-06 13:42:32 +0300

    Update Text_classification.py

--- Text_classification.py
+++ Text_classification.py
@@ -10,25 +10,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 # Training data
-categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+categories = ["alt.atheism", "soc.religion.christian", "comp.graphics", "sci.med"]
 newsgroups_train = fetch_20newsgroups(subset='train',categories=categories)
 
 # Vectorize and normalize
 vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform(newsgroups_train.data)
+train_vectors = vectorizer.fit_transform(newsgroups_train.data)
 
 # Train naive Bayes
 classifier = MultinomialNB()
-classifier.fit(vectors, newsgroups_train.target)
+classifier.fit(train_vectors, newsgroups_train.target)
 
 # Test the classifier with some strings
 test_strings = ["God is love", 
                 "OpenGL on the GPU is fast", 
                 "I'm an atheist", 
                 "Medical students are specialized"]
-test_vector = vectorizer.transform(test_strings)
-predicted = classifier.predict(test_vector)
+test_vectors = vectorizer.transform(test_strings)
+predicted = classifier.predict(test_vectors)
 
 # Print test classification
 for doc, category in zip(test_strings, predicted):
-    print('%r => %s' % (doc, newsgroups_train.target_names[category]))
+    print("%r => %s" % (doc, newsgroups_train.target_names[category]))
