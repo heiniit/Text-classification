11087f0858086b713627f2bbe832fd568382fefc
Heikki Niittylä <31819037+heiniit@users.noreply.github.com>
2018-05-07 21:20:25 +0300

    Added whole book classification examples

--- Classify Finnish texts.py
+++ Classify Finnish texts.py
@@ -9,7 +9,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from pandas import DataFrame
 from urllib.request import urlopen
+from collections import Counter
 
+
+def ClassifyBook(name, url, vectorizer):
+    book = urlopen(url)
+    test_vector = vectorizer.transform(list(book))
+    predicted = classifier.predict(test_vector)
+    counter = Counter(predicted)
+    tot = counter[0]+counter[1]
+    print("\n{0}: {1} percent Kivi, {2} percent London".format(name, 100*counter[0]/tot, 100*counter[1]/tot))
+
+
+# Use a couple of books as a training material
 book1 = urlopen("http://www.gutenberg.org/cache/epub/11152/pg11152.txt") # Nummisuutarit
 df1 = DataFrame(data=list(book1), columns=["data"])
 df1["target"] = 0
@@ -45,14 +57,23 @@ predicted = classifier.predict(test_vector)
 
 # Print test classification
 for test_str, category in zip(test_strings, predicted):
-    print("%r => %s" % (test_str, authors[category]))
-    
+    print("{0} => {1}".format(test_str, authors[category]))
+
+# Classify whole books, line by line
+ClassifyBook("Kihlaus (Kivi)", "http://www.gutenberg.org/cache/epub/12795/pg12795.txt", vectorizer)
+ClassifyBook("Lumikenttien tytär (London)", "http://www.gutenberg.org/cache/epub/47983/pg47983.txt", vectorizer)
+
+
 # Output
 '''
-'Niin muuttuu mailma, Eskoni' => Kivi
-'Käykäämme juoneen, tuottakaamme itsellemme aapiskirjat Hämeenlinnasta ja lähtekäämme lukkarille kouluun, niinkuin on rovastimme käsky.' => Kivi
-'Katsokaat: ellemme osaa lukea, niin laillinen vaimokin on meiltä kielletty hedelmä.' => Kivi
-'Jack Kearns ehdotti pokeria.' => London
-'Kaksi päivää ja kaksi yötä retuutettiin tätä tavaravaunua eteenpäin, pitkässä junajonossa kimakasti viheltävän veturin jäljessä.' => London
-'Eräänä päivänä tuli sinne pieni kurttuinen mies, joka puhui murteellisesti englantia.' => London
+Niin muuttuu mailma, Eskoni => Kivi
+Käykäämme juoneen, tuottakaamme itsellemme aapiskirjat Hämeenlinnasta ja lähtekäämme lukkarille kouluun, niinkuin on rovastimme käsky. => Kivi
+Katsokaat: ellemme osaa lukea, niin laillinen vaimokin on meiltä kielletty hedelmä. => Kivi
+Jack Kearns ehdotti pokeria. => London
+Kaksi päivää ja kaksi yötä retuutettiin tätä tavaravaunua eteenpäin, pitkässä junajonossa kimakasti viheltävän veturin jäljessä. => London
+Eräänä päivänä tuli sinne pieni kurttuinen mies, joka puhui murteellisesti englantia. => London
+
+Kihlaus: 32.6221224031443 percent Kivi, 67.3778775968557 percent London
+
+Lumikenttien tytär: 3.0692301060436242 percent Kivi, 96.93076989395638 percent London
 '''
